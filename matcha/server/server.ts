61ed4618513549fd81b5d7058156a7a4b3520607
hyemch <blossoming33@gmail.com>
2024-07-04 00:42:14 +0900

    [chore] yarn install

--- matcha/server/server.ts
+++ matcha/server/server.ts
@@ -13,7 +13,15 @@ const port: number = 3001;
 // app.listen(port, () => console.log(`${port}`));
 app.listen(port, async() => {
     console.log(`matcha start : ${port}`);
-    AppDataSource.initialize().then(() => {
-        console.log('Database Initialized')
-    }).catch(error => console.log(error));
+
+    try {
+        await AppDataSource.initialize();
+        console.log('Database Initialized');
+    } catch (error) {
+        console.error('Error during Data Source initialization:', error);
+    }
+
+    // AppDataSource.initialize().then(() => {
+    //     console.log('Database Initialized')
+    // }).catch(error => console.log(error));
 })
